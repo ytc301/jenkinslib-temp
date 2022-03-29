4660b76da4497c04969b1c4888b84243edae23ec
ytc301 <ytc301@qq.com>
2022-03-29 11:48:34 +0800

    Create generatePipeline.groovy

---
+++ vars/generatePipeline.groovy
@@ -0,0 +1,28 @@
+def call(String lang){    
+  if (lang == 'java') {
+pipeline {
+    agent any
+    stages {
+        stage('java') {
+            steps {
+
+               sh 'echo java'
+                }
+            }
+        }
+    }
+} else if (lang == 'node'){
+
+pipeline {
+    agent any
+    stages {
+        stage('node') {
+            steps {
+                sh 'echo node'
+            }
+        }
+     }
+   } 
+  }
+  //其他代码
+}
